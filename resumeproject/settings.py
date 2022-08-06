b03a22bdb99b33294af81ae1e6968bc8c932294c
AsHiSh-N <ashish.ashish-coep@bvucoep.edu.in>
2022-08-06 23:55:20 +0530

    4th commit

--- resumeproject/settings.py
+++ resumeproject/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = '+56k^)7e(%vplg3jg-aj$9hzrt=w7_0x5_@^udhmdxrrrg67_j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
